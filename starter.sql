e18c4a48e711c208de4cd64a369364292134ba0c
ariel gabay <o0o0.wvvw.0o0o@gmail.com>
2023-09-20 16:10:13 +0300

    f

--- starter.sql
+++ starter.sql
@@ -2,17 +2,29 @@
 -- 2 התרגולים כאן
 -- תרגול א
 --1
-SELECT Employees.last_name, Employees.first_name, COUNT(order_id)
-FROM Employees
-INNER JOIN Orders
-ON Employees.employee_id = Orders.employee_id GROUP BY Employees.employee_id
+SELECT last_name, first_name, COUNT(order_id) AS total_orders
+FROM employees
+INNER JOIN orders ON employees.employee_id = orders.employee_id
+GROUP BY employees.employee_id
+
 --2
-SELECT categories.category_name, SUM(order_details.quantity*order_details.unit_price*(1-order_details.discount)) AS sum
+SELECT category_name, SUM(quantity*order_details.unit_price*(1-discount)) AS total_sales
 FROM categories 
 INNER JOIN products ON products.category_id = categories.category_id
 INNER JOIN order_details ON products.product_id = order_details.product_id 
-GROUP BY categories.category_name 
-ORDER BY sum DESC;
+GROUP BY category_name 
+ORDER BY total_sales DESC;
+
+--3
+WITH avg_per_order AS(
+    SELECT order_id, AVG(quantity*unit_price*(1-discount))
+    FROM order_details 
+    GROUP BY order_id)
+SELECT company_name, AVG(avg)
+FROM customers INNER JOIN orders ON customers.customer_id = orders.customer_id
+INNER JOIN avg_per_order ON avg_per_order.order_id = orders.order_id
+GROUP BY company_name
+
 --4
 SELECT customers.company_name, SUM(order_details.quantity*order_details.unit_price*(1-order_details.discount)) AS sum
 FROM orders 
@@ -56,31 +68,74 @@ GROUP BY shippers.company_name
 ORDER BY sum DESC
 LIMIT 1
 
---10order_details.unit_price 
--- SELECT *       
--- FROM products
--- INNER JOIN order_details ON products.product_id IN(order_details.product_id) ORDER BY products.product_id
-
-
+--10
+SELECT product_name, reorder_level, products.unit_price   
+FROM products LEFT OUTER JOIN order_details
+ON products.product_id = order_details.product_id
+WHERE order_details.product_id IS NULL
 
 
 -- -- תרגול ב
--- --1
+--1
+SELECT company_name
+FROM customers LEFT OUTER JOIN orders ON customers.customer_id = orders.customer_id
+WHERE orders.order_id IS NULL
 
--- -- 2
-SELECT customers.company_name
-FROM customers 
-WHERE  
+-- 2
+SELECT company_name
+FROM customers INNER JOIN orders ON customers.customer_id = orders.customer_id
+GROUP BY company_name HAVING COUNT(orders.order_id) > 10
 
--- --3
-SELECT product_name FROM products WHERE products.unit_price > (SELECT AVG(unit_price) FROM products)  
- 
+--3
+SELECT product_name
+FROM products
+WHERE products.unit_price > (SELECT AVG(unit_price) FROM products)  
 
--- SELECT * FROM order_details 
+--4
+SELECT product_name
+FROM products LEFT OUTER JOIN order_details ON products.product_id = order_details.product_id
+WHERE order_details.product_id IS NULL
 
- 
--- SELECT * FROM orders 
+--5
+SELECT country
+FROM customers
+GROUP BY country
+HAVING COUNT(country) >= 5
 
--- SELECT * FROM products
- 
-SELECT customer_id FROM customers
+--6
+WITH coustomers_order_1998 AS(
+    SELECT DISTINCT customer_id, order_date 
+    FROM orders
+    WHERE EXTRACT(YEAR FROM order_date) = 1998
+) 
+SELECT company_name
+FROM customers LEFT OUTER JOIN coustomers_order_1998
+ON customers.customer_id = coustomers_order_1998.customer_id
+WHERE coustomers_order_1998.customer_id IS NULL
+
+--7
+SELECT company_name
+FROM customers INNER JOIN orders ON customers.customer_id = orders.customer_id
+WHERE country ILIKE 'France' AND order_date > '1998-01-01'   
+
+--8
+SELECT company_name
+FROM customers INNER JOIN orders ON customers.customer_id = orders.customer_id
+GROUP BY company_name HAVING COUNT(order_id) = 3
+
+--9
+SELECT product_name
+FROM products
+INNER JOIN order_details ON products.product_id = order_details.product_id
+INNER JOIN orders ON orders.order_id = order_details.order_id
+ORDER BY order_date DESC
+LIMIT 1
+
+--10
+SELECT DISTINCT company_name
+FROM suppliers INNER JOIN products ON products.supplier_id = suppliers.supplier_id
+WHERE country ILIKE 'USA'
+
+
+
+
